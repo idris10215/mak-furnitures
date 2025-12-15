fc32e8b42d3216e7cbcefc911e03c2dad1c23e84
idris10215 <mohammedidris10215@gmail.com>
2025-12-15 09:34:33 +0530

    Updated the portfolio section

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -52,7 +52,7 @@ export default function PortfolioPage() {
 
       {/* Grid Gallery */}
       <section className="px-6 max-w-7xl mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 grid-flow-dense">
           {portfolioItems.map((item, index) => (
              <motion.div
               key={item.id}
